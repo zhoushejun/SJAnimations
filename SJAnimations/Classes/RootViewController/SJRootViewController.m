75d4e02443d2882e71144aa5e128a5a0070f6f3b
shejunzhou <965678322@qq.com>
2016-01-30 15:09:13 +0800

    save

--- SJAnimations/Classes/RootViewController/SJRootViewController.m
+++ SJAnimations/Classes/RootViewController/SJRootViewController.m
@@ -73,7 +73,7 @@
     if (!_items) {
         NSArray *array = @[[SJItem itemWithName:@"POP-按钮动画" object:[SJButtonPressViewController class]],
                            [SJItem itemWithName:@"POP-UIView原生简单动画" object:[SJSimpleAnimationViewController class]],
-                           [SJItem itemWithName:@"POP-CABasicAnimation动画" object:[SJBasicAnimationViewController class]],
+                           [SJItem itemWithName:@"POP-基础动画" object:[SJBasicAnimationViewController class]],
                            [SJItem itemWithName:@"POP-关键帧动画" object:[SJKeyframeAnimationViewController class]],
                            [SJItem itemWithName:@"POP-按钮动画" object:[SJButtonPressViewController class]],
                            [SJItem itemWithName:@"POP-按钮动画" object:[SJButtonPressViewController class]],
