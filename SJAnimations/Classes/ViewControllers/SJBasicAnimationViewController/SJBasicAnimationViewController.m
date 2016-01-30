61a09db598061dc95bde1ce2e5bc46e222b50587
shejunzhou <965678322@qq.com>
2016-01-30 14:50:52 +0800

    添加旋转动画

--- SJAnimations/Classes/ViewControllers/SJBasicAnimationViewController/SJBasicAnimationViewController.m
+++ SJAnimations/Classes/ViewControllers/SJBasicAnimationViewController/SJBasicAnimationViewController.m
@@ -35,7 +35,18 @@
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
     UITouch *touch = [touches anyObject];
     CGPoint location = [touch locationInView:self.view];
-    [self transitionAnimation:location];
+    CAAnimation *animation = [_subLayer animationForKey:@"KCBasicAnimation_Translation"];
+    if (animation) {
+        if (0 == _subLayer.speed) {
+            [self animationResume];
+        }else {
+            [self animationPause];
+        }
+    }else {
+        [self transitionAnimation:location];
+        [self rotationAnimation];
+        
+    }
 }
 
 - (void)transitionAnimation:(CGPoint)location {
@@ -60,6 +71,7 @@
     [CATransaction setDisableActions:YES];//禁用隐式动画
     _subLayer.position = [[anim valueForKey:@"KCBasicAnimationLocation"] CGPointValue];
     [CATransaction commit];//提交事务
+    [self animationPause];
 }
 
 #pragma mark - getter and setter
@@ -72,6 +84,34 @@
         _subLayer.contents = (__bridge id _Nullable)([UIImage imageNamed:@"photo.png"].CGImage);
     }
     return _subLayer;
+}
+
+#pragma mark - private
+
+/** 旋转动画 */
+- (void)rotationAnimation {
+    CABasicAnimation *basicAnimation = [CABasicAnimation animationWithKeyPath:@"transform.rotation.z"];
+    basicAnimation.toValue = [NSNumber numberWithFloat:M_PI_2 * 3];
+    basicAnimation.duration = 6.0;
+    basicAnimation.autoreverses = YES;
+    //    basicAnimation.repeatCount = HUGE_VALF;//设置无限循环
+    basicAnimation.removedOnCompletion = NO;
+    [_subLayer addAnimation:basicAnimation forKey:@"KCBasicAnimation_Rotation"];
+}
+
+/** 动画暂停 */
+- (void)animationPause {
+    CFTimeInterval interval = [_subLayer convertTime:CACurrentMediaTime() fromLayer:nil];
+    [_subLayer setTimeOffset:interval];
+    _subLayer.speed = 0;
+}
+
+/** 动画恢复 */
+- (void)animationResume {
+    CFTimeInterval beginTime = CACurrentMediaTime() - _subLayer.timeOffset;
+    _subLayer.timeOffset = 0;
+    _subLayer.beginTime = beginTime;
+    _subLayer.speed = 1.0;
 }
 
 @end
